edc2d6a988a4d49c7d50fb499efa2e51d60f4f04
Мария <114505489+MariAntonova94@users.noreply.github.com>
2023-10-04 13:06:45 +0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -4,11 +4,9 @@ from typing import List
 def binary_search(arr: List[int], number: int) -> int:
     """
     Реализует бинарный поиск элемента в целочисленном массиве.
-
     Agrs:
     - arr (List[int]): Целочисленный массив, в котором осуществляется поиск.
     - number (int): Искомый элемент в массиве.
-
     Return:
     - int: Индекс элемента в массиве, если элемент найден. Или -1, если элемент не найден.
     """
@@ -17,17 +15,13 @@ def binary_search(arr: List[int], number: int) -> int:
 
     while left <= right:
         mid = (left + right) // 2
-
         if arr[mid] == number:
             return mid
         elif arr[mid] < number:
             left = mid + 1
         else:
             right = mid - 1
-
     return -1
-
-
 def main():
     # На вход подаем целочисленный массив
     arr = [1, 4, 7, 6, 17, 3, 12]
@@ -35,12 +29,9 @@ def main():
     print(f"Исходный массив: {arr}")
     number = int(input("Введите искомый элемент: "))
     result = binary_search(arr, number)
-
     if result == -1:
         print("Искомого элемента нет в массиве")
     else:
         print(f"Искомый элемент: {number} найден по индексу: {result}")
-
-
 if __name__ == "__main__":
     main()
